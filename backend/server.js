66ce813c27f0685a171b0edc56b6813290c19ef4
Bastin <jdbastinms@gmail.com>
2026-06-29 11:51:26 +0530

    Add additional CORS origin for localhost

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ connectDB();
 // Middleware
 app.use(cors({
   origin: [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://localhost:5174/'
   ]
 }));
 app.use(express.json());
